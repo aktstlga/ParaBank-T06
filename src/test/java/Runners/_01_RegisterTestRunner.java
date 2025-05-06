e11b684f547d6639c87ed4d47fdb1874a41713ff
aktstlga <akts.tlga@gmail.com>
2025-05-06 18:30:53 +0300

    Finished the general.

---
+++ src/test/java/Runners/_01_RegisterTestRunner.java
@@ -0,0 +1,29 @@
+package Runners;
+
+
+import com.aventstack.extentreports.service.ExtentService;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+
+@CucumberOptions(
+        features = {"src/test/java/FeatureFiles/_01_Register.feature"},
+        glue = {"StepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+public class _01_RegisterTestRunner extends AbstractTestNGCucumberTests {
+    @AfterClass
+    public  static void writeExtendReport()
+    {
+        ExtentService.getInstance().setSystemInfo("Windows User Name", System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("User Name", "Tolga Aktaş");
+        ExtentService.getInstance().setSystemInfo("Application Name", "ParaBank");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department", "QA");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+    }
+}
